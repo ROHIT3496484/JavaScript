85e0e31e4a3d3628a1f7f3829ec80ed404107105
Rohit Rana <84340668+ROHIT3496484@users.noreply.github.com>
2025-04-17 14:00:57 +0530

    Create mappolyfil.js

---
+++ mappolyfil/mappolyfil.js
@@ -0,0 +1,20 @@
+var arr = [1, 2, 3, 4];
+
+/**arr.map((item) => {
+  console.log(item * 2);
+});**/
+
+Array.prototype.mymap = function (callback) {
+  if (!callback) {
+    throw Error("you are not using callback");
+  }
+  let newarr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const result = callback(arr[i], i, arr);
+    newarr.push(result);
+  }
+};
+
+arr.mymap((item) => {
+  console.log(item * 3);
+});
